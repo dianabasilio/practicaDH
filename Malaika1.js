65a85a8c637b48f6c09627ec87475edb09f00d9f
malaikaandrade <malaikaandrade@gmail.com>
2021-07-13 21:26:03 -0500

    ya se arregló el conflicto

--- Malaika1.js
+++ Malaika1.js
@@ -2,3 +2,4 @@ console.log("Hola soy el tercer archivo del repositorios");
 console.log("¡Hola Malaika!")
 console.log("¡Hola Diana!");
 console.log("ya se arregló")
+console.log("Si pudimos hacer la práctica de hoy!")
